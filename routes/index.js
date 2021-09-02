ae920b7b945a75a87013ba7c79f248d0b4d2a39a
KaustubhDubey <dubeykaustubh07@gmail.com>
2021-09-02 22:46:09 +0530

    Initial Authors Routes

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,8 @@ const { Router } = require('express')
 const express = require('express')
 const router = express.Router()
 
-router.get('/', (req, res)=> {
+router.get('/', (req, res) => {
     res.render('index')
-}) 
-    
+})
+
 module.exports = router
